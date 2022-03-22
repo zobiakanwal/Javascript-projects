9b5103fa5603bee3e72f4ac0f952848e539bf3b5
zobiakanwal <xobiaaijaz.com>
2022-03-22 17:33:41 +0500

    updated

--- Project 1/app.js
+++ Project 1/app.js
@@ -4,3 +4,9 @@ const body = document.querySelector('body');
 let color = ['red', 'yellow', 'blue', 'green'];
 
 body.style.backgroundColor = "pink";
+
+button.addEventListener('click', changeBG);
+
+function changeBG(){
+    body.style.backgroundColor = color[2];
+}
