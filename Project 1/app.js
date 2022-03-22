4bc71772917461821d76cf9072b1c2a61d511faf
zobiakanwal <xobiaaijaz.com>
2022-03-22 17:15:49 +0500

    updated

---
+++ Project 1/app.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+
+let color = ['red', 'yellow', 'blue', 'green'];
+
+body.style.backgroundColor = "pink";
